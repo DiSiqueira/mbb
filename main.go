da13fb78e04114d1934c232f375934d6be3a5566
Diego Siqueira <dsi@hellofresh.com>
2017-10-24 23:44:30 +0200

    buy function

--- main.go
+++ main.go
@@ -18,17 +18,65 @@ func main() {
 		log.Fatal(err)
 	}
 
+	for {
+		if err := sell(api); err != nil {
+			log.Fatal(err)
+		}
+		if err := buy(api); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("finished")
+}
+
+func buy(api *kraken) error {
+	smallerPrice := api.lastBuy
+	wait := true
+	for wait {
+		ticker, err := api.Ticker()
+		if err != nil {
+			return err
+		}
+
+		p, err := strconv.ParseFloat(ticker.Close[0], 32)
+		if err != nil {
+			return err
+		}
+		lastPrice := float32(p)
+		diff := lastPrice / smallerPrice
+
+		if diff > 1.01 {
+			wait = false
+		}
+
+		if lastPrice < smallerPrice {
+			smallerPrice = lastPrice
+		}
+
+		fmt.Printf("Last Price: %f Bigger Price: %f Difference: %f \n", lastPrice, smallerPrice, diff)
+		time.Sleep(time.Second * 4)
+	}
+
+	if err := api.Buy(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func sell(api *kraken) error {
 	biggerPrice := api.lastBuy
 	hold := true
 	for hold {
 		ticker, err := api.Ticker()
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 
 		p, err := strconv.ParseFloat(ticker.Close[0], 32)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		lastPrice := float32(p)
 		diff := lastPrice / biggerPrice
@@ -46,8 +94,8 @@ func main() {
 	}
 
 	if err := api.Sell(); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	fmt.Println("finished")
+	return nil
 }
